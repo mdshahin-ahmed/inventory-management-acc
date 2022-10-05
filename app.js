12f4c0da77afcd2b77bd0fbf4ace752ab8242cae
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-10-05 23:21:23 +0600

    Done login

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const categoryRoute = require("./routs/category.route");
 const storeRoute = require("./routs/store.route");
 const supplierRoute = require("./routs/supplier.route");
 const stockRoute = require("./routs/stock.route");
+const userRoute = require("./routs/user.route");
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
@@ -25,5 +26,6 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/store", storeRoute);
 app.use("/api/v1/supplier", supplierRoute);
 app.use("/api/v1/stock", stockRoute);
+app.use("/api/v1/user", userRoute);
 
 module.exports = app;
